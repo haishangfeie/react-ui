9ff8300cf92961988bf46e74993f1b258168f4f2
wenxingjie <26127631+haishangfeie@users.noreply.github.com>
2020-11-24 16:48:11 +0800

    feat: 开始按钮组件的开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button, { ButtonTypes, ButtonSizes } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button disabled>Hello</Button>
+      <Button disabled btnType={ButtonTypes.primary}>
+        Hello
+      </Button>
+      <Button href="https://www.baidu.com" disabled btnType={ButtonTypes.link}>
+        百度 link
+      </Button>
+      <Button disabled btnType={ButtonTypes.danger}>
+        Hello
+      </Button>
+
+      <Button size={ButtonSizes.lg}>Hello</Button>
+      <Button size={ButtonSizes.md}>Hello</Button>
+      <Button size={ButtonSizes.sm}>Hello</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode } from 'react'
+import classnames from 'classnames'
+
+export enum ButtonSizes {
+  lg = 'lg',
+  md = 'md',
+  sm = 'sm',
+}
+
+export enum ButtonTypes {
+  default = 'default',
+  primary = 'primary',
+  danger = 'danger',
+  link = 'link',
+}
+
+interface baseButtonProps {
+  className?: string
+  children?: ReactNode
+  btnType?: ButtonTypes
+  size?: ButtonSizes
+  disabled?: boolean
+  href?: string
+}
+
+const Button: React.FC<baseButtonProps> = (props) => {
+  const { children, btnType, size, href, disabled } = props
+  const clsName = classnames('react-ui-button', {
+    [`react-ui-button--${btnType}`]: true,
+    [`react-ui-button--${size}`]: true,
+    'react-ui-button--disabled': disabled,
+  })
+  if (btnType === ButtonTypes.link) {
+    return (
+      <a href={href} className={clsName}>
+        {children}
+      </a>
+    )
+  }
+
+  return (
+    <button disabled={disabled} className={clsName}>
+      {children}
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  size: ButtonSizes.lg,
+  btnType: ButtonTypes.default,
+  disabled: false,
+  children: '',
+  className: '',
+}
+
+export default Button
